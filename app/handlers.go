7bd8be415716193767cc1ff8881e83474ff0f858
Michael Street <mi.street@gmail.com>
2021-11-03 12:59:55 +0200

    comments

--- app/handlers.go
+++ app/handlers.go
@@ -14,19 +14,24 @@ type CustomerHandlers struct {
 }
 
 func (ch *CustomerHandlers) getAllCustomers(w http.ResponseWriter, r *http.Request) {
-
+	/* access the customer repository via the service */
 	customers, _ := ch.service.GetAllCustomers()
 
+	/* handle the response from the service */
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(customers)
 
 }
 
 func (ch *CustomerHandlers) getCustomer(w http.ResponseWriter, r *http.Request) {
-
+	/* fetch URL params */
 	vars := mux.Vars(r)
 	id := vars["customer_id"]
+
+	/* access the customer repository via the service */
 	customer, err := ch.service.GetCustomer(id)
+
+	/* handle the response from the service */
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, err.Error())
