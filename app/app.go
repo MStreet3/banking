08f2623a3c0f9d900f9c98992eaaef3905b6d069
Michael Street <mi.street@gmail.com>
2021-11-03 11:54:29 +0200

    add in db

--- app/app.go
+++ app/app.go
@@ -1,9 +1,13 @@
 package app
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func Start() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/customers", getAllCustomers)
-	http.ListenAndServe("localhost:8080", mux)
+	router := mux.NewRouter()
+	router.HandleFunc("/customers", getAllCustomers)
+	http.ListenAndServe("localhost:8080", router)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mstreet3/banking
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
